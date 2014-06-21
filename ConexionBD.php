68b9f37d617afc84e5304c9017216f1d4d577a30
MatamorosF3 <matamorosf3@gmail.com>
2014-06-21 11:21:13 -0600

    clase conexion

---
+++ ConexionBD.php
@@ -0,0 +1,10 @@
+<?php
+// Conectando, seleccionando la base de datos
+$link = mysql_connect('127.0.0.1', 'root', 'fuckeveryonee')
+    or die('No se pudo conectar: ' . mysql_error());
+//echo 'Connected successfully';
+mysql_select_db('torneo') or die('No se pudo seleccionar la base de datos');
+?>
+
+
+
